6a33185caac5fe85fba8cb2a6fe0f6615478f627
Damian Federico <damianfcardozo@gmail.com>
2020-06-01 17:05:27 -0300

    Add run left/right animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,29 +1,28 @@
 extends KinematicBody2D
 
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+
 var velocity = Vector2.ZERO
+# the $ sing is a shorthand to getting access to a node in the three
+# that is part of the same scene 
+onready var animationPlayer = $AnimationPlayer
 
 func _physics_process(delta):
-
-# Doing this it works but not move diagonally
-#	if Input.is_action_pressed("ui_right"):
-#		velocity.x = 4
-#	elif Input.is_action_pressed("ui_left"):
-#		velocity.x = -4 
-#	elif Input.is_action_pressed("ui_up"):
-#		velocity.y = -4
-#	elif Input.is_action_pressed("ui_down"):
-#		velocity.y = 4 
-#	else: 
-#		velocity.x = 0
-#		velocity.y = 0
-
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")		
-
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
 	if input_vector != Vector2.ZERO: 
-		velocity = input_vector
-	else: 
-		velocity =  Vector2.ZERO
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationPlayer.play("IdleRight") 
+		velocity =  velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
