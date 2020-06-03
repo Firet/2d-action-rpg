15ee792cbc8b6dfe319518537c3a07a047101609
Damian Federico <damianfcardozo@gmail.com>
2020-06-03 15:20:16 -0300

    Add attack animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,12 +4,30 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 # the $ sing is a shorthand to getting access to a node in the three
 # that is part of the same scene 
 onready var animationPlayer = $AnimationPlayer
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+			
+		ROLL: 
+			pass
+		
+		ATTACK: 
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -26,3 +44,10 @@ func _physics_process(delta):
 		velocity =  velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
+	
+	## TO DO: add animation tree with attack
+	##if Input.is_action_just_pressed("attack"):
+	##	state = ATTACK
+	
+func attack_state(delta):
+	pass
