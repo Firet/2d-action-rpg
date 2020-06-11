388a8157494ede247f97cc491985a773262e1f6a
Damian Federico <damianfcardozo@gmail.com>
2020-06-11 17:03:23 -0300

    Create hitboxes for sword and grass

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://Effects/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		# Assign the global position of Grass to the animated grass position
-		grassEffect.global_position = global_position
-		queue_free()
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	# Assign the global position of Grass to the animated grass position
+	grassEffect.global_position = global_position
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect() 
+	# Disable hurbox to avoid hit multiple times (one hit kill)
+	queue_free()
