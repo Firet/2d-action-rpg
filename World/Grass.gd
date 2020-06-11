b1941b130afa4000c6e559451b335ce5da58c563
Damian Federico <damianfcardozo@gmail.com>
2020-06-11 14:24:58 -0300

    Create grass effect

--- World/Grass.gd
+++ World/Grass.gd
@@ -2,4 +2,10 @@ extends Node2D
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
+		var GrassEffect = load("res://Effects/GrassEffect.tscn")
+		var grassEffect = GrassEffect.instance()
+		var world = get_tree().current_scene
+		world.add_child(grassEffect)
+		# Assign the global position of Grass to the animated grass position
+		grassEffect.global_position = global_position
 		queue_free()
