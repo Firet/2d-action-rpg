5390a00ac52952172d93faffffd10a181da5aa17
Damian Federico <damianfcardozo@gmail.com>
2020-06-17 20:25:37 -0300

    Use setter to set bat's health

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -11,7 +11,8 @@ func _physics_process(delta):
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
-	stats.health -= 1
-	if stats.health <= 0:
-		queue_free()
+	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
+
+func _on_Stats_no_health():
+	queue_free()
--- Stats.gd
+++ Stats.gd
@@ -1,4 +1,20 @@
 extends Node
 
 export(int) var max_health = 1
-onready var health = max_health
+onready var health = max_health setget set_health
+ 
+# This is a way to check for no health
+# but its not performant, because it asking
+# every frame about health, 
+# it better using setters and getters
+#func _process(delta):
+#	if health <=0:
+#		emit_signal("no_health")
+
+signal no_health
+
+
+func set_health(value):
+	health = value
+	if health <= 0:
+		emit_signal("no_health")
