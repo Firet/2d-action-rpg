8ad89c39db0c87f59dd11327be74fa46daf6cb37
Damian Federico <damianfcardozo@gmail.com>
2020-06-19 16:16:55 -0300

    Enemy chase player. Amazing!

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,15 +2,60 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+# Bat has low friction because is in the air, 
+# 200 is a low number compared to the player's friction
+export var FRICTION = 200
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
+var state = CHASE
+
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
 
 func _physics_process(delta):
-	# Bat has low friction because is in the air, 
-	# 200 is a low number compared to the player's friction
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+		
+		WANDER:
+			pass
+		
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				# Subtract player position less enemy position. 
+				# The center is position (0,0)
+				# When is normalized and the velocity linked to 1. 
+				var direction = (player.global_position - global_position).normalized()
+				# Move towards the player. Magic!
+				# Velocity in direction is 1 (because was normalized).
+				# and then multiplied to max speed.
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else: 
+				state = IDLE
+				
+			sprite.flip_h = velocity.x < 0
+
+
+	velocity = move_and_slide(velocity)
+		
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
