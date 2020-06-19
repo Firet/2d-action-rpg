220e41ae4552f32ac6e54c4a14c764c0a94612ef
Damian Federico <damianfcardozo@gmail.com>
2020-06-19 11:58:47 -0300

    Add Enemy death effect

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,11 +1,13 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
 var knockback = Vector2.ZERO
 
 onready var stats = $Stats
 
 func _physics_process(delta):
-	#  Bat has low friction because is in the air, 
+	# Bat has low friction because is in the air, 
 	# 200 is a low number compared to the player's friction
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
@@ -16,3 +18,7 @@ func _on_Hurtbox_area_entered(area):
 
 func _on_Stats_no_health():
 	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	# enemyDeathEffect is attach to the ysort (bat's parent)
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
--- World/Grass.gd
+++ World/Grass.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
 func create_grass_effect():
-	var GrassEffect = load("res://Effects/GrassEffect.tscn")
 	var grassEffect = GrassEffect.instance()
-	var world = get_tree().current_scene
-	world.add_child(grassEffect)
+	# grassEffect is attached to Grass node
+	# (which is the parent of all individual grass nodes)
+	get_parent().add_child(grassEffect)
 	# Assign the global position of Grass to the animated grass position
 	grassEffect.global_position = global_position
 
