22132f27fceaca272f805169fc7b0b76b1132e6b
Firet <damianfcardozo@gmail.com>
2020-06-20 19:38:38 -0300

    Add hearts icons as HP indicator

--- Stats.gd
+++ Stats.gd
@@ -1,7 +1,7 @@
 extends Node
 
-export(int) var max_health = 1
-onready var health = max_health setget set_health
+export(int) var max_health = 1 setget set_max_health
+var health = max_health setget set_health
  
 # This is a way to check for no health
 # but its not performant, because it asking
@@ -13,9 +13,21 @@ onready var health = max_health setget set_health
 
 signal no_health
 signal health_changed(value)
+signal max_health_changed(value)
 
+func set_max_health(value):
+	max_health = value
+	# Check that actual health cannot ever
+	# be larger than max health
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
 func set_health(value):
 	health = value
 	emit_signal("health_changed", health)
 	if health <= 0:
 		emit_signal("no_health")
+
+# This function is created to solve the imposibility 
+# to make health onready 
+func _ready():
+	self.health = max_health
--- UI/HealthUI.gd
+++ UI/HealthUI.gd
@@ -3,17 +3,25 @@ extends Control
 var hearts = 4 setget set_hearts
 var max_hearts = 4 setget set_max_hearts
 
-onready var label = $Label
+onready var heartUIFull = $HeartUIFull
+onready var heartUIEmpty = $HeartUIEmpty
 
 func set_hearts(value):
 	hearts = clamp(value, 0, max_hearts)
-	if label != null:
-		label.text = "HP = " + str(hearts)
+	if heartUIFull != null:
+		# 15 is the size of the heart texture
+		heartUIFull.rect_size.x = hearts * 15
 
 func set_max_hearts(value):
 	max_hearts = max(value, 1)
+	# Check that actual health cannot ever
+	# be larger than max health.
+	self.hearts = min(hearts, max_hearts)
+	if heartUIEmpty != null:
+		heartUIEmpty.rect_size.x = max_hearts * 15 
 
 func _ready():
 	self.max_hearts = PlayerStats.max_health
 	self.hearts = PlayerStats.health
 	PlayerStats.connect("health_changed", self, "set_hearts")
+	PlayerStats.connect("max_health_changed", self, "set_max_hearts")	
